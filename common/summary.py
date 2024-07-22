2b7c690184cf769bcf277c62c4f09408b8da1ed1
hudua <40040433+hudua@users.noreply.github.com>
2024-07-22 09:39:01 -0400

    Update summary.py

--- common/summary.py
+++ common/summary.py
@@ -5,7 +5,8 @@ import uuid
 def generate_prompt(prompt, system_message, aoi_key, can_aoai_url, model, aoai_version_completion):
     
     # Define the deployment name and base URL for Azure OpenAI service  
-
+    base_url=f"{can_aoai_url}/openai/deployments/{model}/chat/completions?api-version={aoai_version_completion}"
+    
     headers = {  
         "Content-Type": "application/json",  
         "api-key": aoi_key
